ee746d67f1cc0e242fd05d6ba89f67a54119aeb8
nabilaaisa72 <162269708+nabilaaisa72@users.noreply.github.com>
2024-06-10 18:45:01 +0700

    backend-6

---
+++ controllers/mahasiswa.js
@@ -0,0 +1,65 @@
+const Mahasiswa = require("../models/mahasiswa");
+
+module.exports = {
+    insert: async (req, res) => {
+        const data = new Mahasiswa({
+            nim: req.body.nim,
+            nama: req.body.nama,
+            angkatan: req.body.angkatan,
+            prodi: req.body.prodi,
+        });
+
+        try {
+            const dataToSave = await data.save();
+            res.status(200).json(dataToSave);
+        } catch (error) {
+            res.status(400).json({ message: error.message });
+        }
+    },
+
+    getMahasiswa: async (req,res) => {
+        try {
+            const data = await Mahasiswa.find();
+            res.json(data)
+        } catch (error) {
+            res.status(500).json({ message: error.message})
+        }
+    },
+
+    getMahasiswaByNim: async (req,res) => {
+        const nim = req.params.nim
+        try {
+            const data = await Mahasiswa.find().where('nim').equals(nim);
+            res.json(data);
+        } catch (error) {
+            res.status(500).json({message: error.message})
+        }
+    },
+
+    update: async(req,res) => {
+        const filter = {nim: req.params.nim}
+        const updatedData = {
+            nim : req.params.nim,
+            nama : req.body.nama,
+            angkatan : req.body.angkatan,
+            prodi : req.body.prodi,
+        }
+        try {
+            await Mahasiswa.updateOne(filter, updatedData)
+            res.status(200).json(updatedData)
+        
+        } catch (error) {
+            res.status(409).json({message: error.message})
+        }
+    },
+
+    delete: async (req,res) => {
+        const filter = {nim: req.params.nim}
+        try {
+            await Mahasiswa.deleteOne(filter)
+            res.send("data telah terhapus")
+        } catch (error) {
+            res.status(409).json({message: error.message})
+        }
+    },
+}
